6450739885e62c1f5830c033f28efb3a4bd66075
Jonathan Lancaster <jcast90@me.com>
2017-05-06 14:53:42 -0700

    add part3 part4

---
+++ part3/json.js
@@ -0,0 +1,26 @@
+/**
+ * Created by jon on 5/2/17.
+ */
+{ 'team': [
+    {
+        'name': 'Robert E. Patterson',
+        'role': 'Manager'
+    },
+    {
+        'name': 'Thaddeus Mandingo',
+        'role': 'IT Dude'
+    },
+    {
+        'name': 'Sarah E. Lee',
+        'role': 'Solutions Expert'
+    },
+    {
+        'name': 'Fredrick Neitsche',
+        'role': 'Solutions Philosopher'
+    },
+    {
+        'name': 'Bob',
+        'role': 'He makes the burgers'
+    }
+]};
+
